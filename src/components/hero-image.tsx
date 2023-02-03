897c68571f17415f47dc5a3c5e35177b0a5caebd
srikar-samudrala <srikar@coto.world>
2023-02-03 12:23:19 +0530

    adds skewness to image container

--- src/components/hero-image.tsx
+++ src/components/hero-image.tsx
@@ -1,11 +1,9 @@
 export function HeroImage() {
   return (
-    <div>
-      <img
-        src="/img/hero.webp"
-        alt="hero image"
-        className="mt-[12.8rem] opacity-0"
-      />
+    <div className="mt-[12.8rem] overflow-hidden [perspective:2000px] xl:overflow-visible">
+      <div className="rounded-lg border border-transparent-white bg-white bg-opacity-[0.01] bg-hero-gradient [transform:rotateX(25deg)]">
+        <img src="/img/hero.webp" alt="hero image" />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -54,6 +54,8 @@ module.exports = {
         "linear-gradient(92.88deg, rgb(69, 94, 181) 9.16%, rgb(86, 67, 204) 43.89%, rgb(103, 63, 215) 64.72%)",
       "page-gradient":
         "radial-gradient(ellipse 80% 50% at 50% -20%,rgba(120,119,198,0.3),transparent)",
+      "hero-gradient":
+        "radial-gradient(ellipse 50% 80% at 20% 40%,rgba(93,52,221,0.1),transparent), radial-gradient(ellipse 50% 80% at 80% 50%,rgba(120,119,198,0.15),transparent)",
     },
     boxShadow: {
       primary: "rgb(80 63 205 / 50%) 0px 1px 40px",
