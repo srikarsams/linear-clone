2f05939832ac6232f86c34f62fd9e82c34428e23
srikar-samudrala <srikar@coto.world>
2023-02-06 18:05:34 +0530

    adds stars horizon gradient section

---
+++ src/components/clients.tsx
@@ -0,0 +1,41 @@
+import { AlanIcon } from "./icons/alan-icon";
+import { ArcIcon } from "./icons/arc-icon";
+import { CashAppIcon } from "./icons/cashapp-icon";
+import { DescriptIcon } from "./icons/descript-icon";
+import { LoomIcon } from "./icons/loom-icon";
+import { MercuryIcon } from "./icons/mercury-icon";
+import { OpenseaIcon } from "./icons/opensea-icon";
+import { PitchIcon } from "./icons/pitch-icon";
+import { RampIcon } from "./icons/ramp-icon";
+import { RaycastIcon } from "./icons/raycast-icon";
+import { RetoolIcon } from "./icons/retool-icon";
+import { VercelIcon } from "./icons/vercel-icon";
+
+export function Clients() {
+  return (
+    <>
+      <p className="mb-12 text-center text-lg text-white md:text-xl">
+        <span className="text-primary-text">
+          Powering the world's best product teams.
+        </span>
+        <br className="hidden md:block" />
+        From next-gen startups to established enterprises.
+      </p>
+
+      <div className="flex flex-wrap justify-around gap-x-6 gap-y-8 [&_svg]:max-w-[16rem] [&_svg]:basis-[calc(50%-12px)] md:[&_svg]:basis-[calc(16.66%-20px)]">
+        <RampIcon />
+        <LoomIcon className="hidden md:block" />
+        <VercelIcon />
+        <DescriptIcon className="hidden md:block" />
+        <CashAppIcon />
+        <RaycastIcon />
+        <MercuryIcon />
+        <RetoolIcon />
+        <AlanIcon className="hidden md:block" />
+        <ArcIcon className="hidden md:block" />
+        <OpenseaIcon className="hidden md:block" />
+        <PitchIcon className="hidden md:block" />
+      </div>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,73 @@
-import type { NextPage } from "next";
+import classNames from "classnames";
 
-import { Button, IconWrapper } from '../components/button';
-import Container from '../components/container';
-import { Hero, HeroSubTitle, HeroTitle } from '../components/hero';
-import { HeroImage } from '../components/hero-image';
-import { ChevronIcon } from '../components/icons/chevron-icon';
+import { Button, IconWrapper } from "../components/button";
+import { Clients } from "../components/clients";
+import Container from "../components/container";
+import { Hero, HeroSubTitle, HeroTitle } from "../components/hero";
+import { HeroImage } from "../components/hero-image";
+import { ChevronIcon } from "../components/icons/chevron-icon";
+import { StarHorizon } from "../components/icons/star-horizon";
+
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="overflow-hidden">
-      <Container className="pt-[6.4rem]">
-        <Hero>
-          <Button
-            variant="secondary"
-            size="small"
-            href="#"
-            className="flex animate-fade-in"
-          >
-            Linear 2022 Release - Built for scale <IconWrapper>→</IconWrapper>
-          </Button>
-          <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-            Linear is a better way
-            <br className="hidden md:block" /> to build products
-          </HeroTitle>
-          <HeroSubTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-            Meet the new standard for modern software development.
-            <br className="hidden md:block" />
-            Streamline issues, sprints, and product roadmaps.
-          </HeroSubTitle>
-          <Button
-            href="/"
-            variant="primary"
-            size="large"
-            className="flex translate-y-[-1rem] animate-fade-in items-center opacity-0 [--animation-delay:600ms]"
-          >
-            Get Started
-            <IconWrapper>
-              <ChevronIcon />
-            </IconWrapper>
-          </Button>
-          <HeroImage />
-        </Hero>
+    <>
+      <div className="overflow-hidden pb-[16.4rem] md:pb-[25.6rem]">
+        <Container className="pt-[6.4rem]">
+          <Hero>
+            <Button
+              variant="secondary"
+              size="small"
+              href="#"
+              className="flex animate-fade-in"
+            >
+              Linear 2022 Release - Built for scale <IconWrapper>→</IconWrapper>
+            </Button>
+            <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+              Linear is a better way
+              <br className="hidden md:block" /> to build products
+            </HeroTitle>
+            <HeroSubTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+              Meet the new standard for modern software development.
+              <br className="hidden md:block" />
+              Streamline issues, sprints, and product roadmaps.
+            </HeroSubTitle>
+            <Button
+              href="/"
+              variant="primary"
+              size="large"
+              className="flex translate-y-[-1rem] animate-fade-in items-center opacity-0 [--animation-delay:600ms]"
+            >
+              Get Started
+              <IconWrapper>
+                <ChevronIcon />
+              </IconWrapper>
+            </Button>
+            <HeroImage />
+          </Hero>
+        </Container>
+      </div>
+
+      <Container>
+        <Clients />
       </Container>
-    </div>
+
+      <div
+        className={classNames(
+          "mask-stars-horizon relative my-[-12.8rem] h-[60rem] overflow-hidden",
+          "inset-0 bg-stars-gradient before:absolute before:opacity-40",
+          "after:absolute after:top-1/2 after:-left-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[50%] after:border-t-[rgba(120,119,198,0.4)] after:bg-background"
+        )}
+        style={
+          {
+            "--color": "#7877C6",
+          } as React.CSSProperties
+        }
+      >
+        <StarHorizon />
+      </div>
+    </>
   );
 };
 
